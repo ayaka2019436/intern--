5d18427540cf6c79eddd18b29bc4d9a909aa3d58
薄井彩花 <>
2023-03-13 19:26:34 -0700

    画面外側を

--- intern-book-app/src/app/app.module.ts
+++ intern-book-app/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { MatCardModule } from '@angular/material/card';
+import { ListComponent } from './pages/list/list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+const ROUTE_TABLE: Routes = [{ path: '', component: ListComponent }];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, MatCardModule],
+  declarations: [AppComponent, ListComponent],
+  imports: [
+    BrowserModule,
+    MatCardModule,
+    RouterModule.forRoot(ROUTE_TABLE), // 追加. routing の情報を登録する
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
